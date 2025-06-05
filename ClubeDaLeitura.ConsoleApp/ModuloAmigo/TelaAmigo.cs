64577b99c51a07ad3139ff7698e0ea3ad7cb05fa
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-06-05 11:54:59 -0300

    Adicionada validação de dados duplicados para cadastro de amigo

--- ClubeDaLeitura.ConsoleApp/ModuloAmigo/TelaAmigo.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloAmigo/TelaAmigo.cs
@@ -8,6 +8,65 @@ public class TelaAmigo : TelaBase
     {
     }
 
+    public override void CadastrarRegistro()
+    {
+        ExibirCabecalho();
+
+        Console.WriteLine($"Cadastro de {nomeEntidade}");
+
+        Console.WriteLine();
+
+        Amigo novoRegistro = (Amigo)ObterDados();
+
+        string erros = novoRegistro.Validar();
+
+        if (erros.Length > 0)
+        {
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(erros);
+            Console.ResetColor();
+
+            Console.Write("\nDigite ENTER para continuar...");
+            Console.ReadLine();
+
+            CadastrarRegistro();
+
+            return;
+        }
+
+        EntidadeBase[] registros = repositorio.SelecionarRegistros();
+
+        for (int i = 0; i < registros.Length; i++)
+        {
+            Amigo amigoRegistrado = (Amigo)registros[i];
+
+            if (amigoRegistrado == null)
+                continue;
+
+            if (amigoRegistrado.Nome == novoRegistro.Nome || amigoRegistrado.Telefone == novoRegistro.Telefone)
+            {
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Um amigo com este nome ou telefone já foi cadastrado!");
+                Console.ResetColor();
+
+                Console.Write("\nDigite ENTER para continuar...");
+                Console.ReadLine();
+
+                CadastrarRegistro();
+                return;
+            }
+        }
+
+        repositorio.CadastrarRegistro(novoRegistro);
+
+        Console.WriteLine($"\n{nomeEntidade} cadastrado com sucesso!");
+        Console.ReadLine();
+    }
+
     public override void VisualizarRegistros(bool exibirCabecalho)
     {
         if (exibirCabecalho == true)
